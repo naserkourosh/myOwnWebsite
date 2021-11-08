a1b3714221d075a85ec653a0fe1b3f0db00fcfce
Naser Kourosh <naserkorosh1986@gmail.com>
2021-11-08 22:44:07 +0330

    portfolio added

--- js/app.js
+++ js/app.js
@@ -12,37 +12,37 @@ tabsContainer.addEventListener("click", (e) => {
 })
 // -----------------Portfolio Popup----------------
 
-document.addEventListener('click' , (e)=>{
-    if(e.target.classList.contains('view-project-btn')){
-        togglePortfolioPopup();
-        document.querySelector('.portfolio-popup').scrollTo(0,0);
-        portfolioItemDetails(e.target.parentElement);
-    }
-})
-function togglePortfolioPopup () { 
-    document.querySelector('.portfolio-popup').classList.toggle('open');
-        document.body.classList.toggle('hide-scrolling');
-        document.querySelector('.main').classList.toggle('fade-out');
- }
-document.querySelector('.pp-close').addEventListener('click' , togglePortfolioPopup);
+// document.addEventListener('click' , (e)=>{
+//     if(e.target.classList.contains('view-project-btn')){
+//         togglePortfolioPopup();
+//         document.querySelector('.portfolio-popup').scrollTo(0,0);
+//         portfolioItemDetails(e.target.parentElement);
+//     }
+// })
+// function togglePortfolioPopup () { 
+//     document.querySelector('.portfolio-popup').classList.toggle('open');
+//         document.body.classList.toggle('hide-scrolling');
+//         document.querySelector('.main').classList.toggle('fade-out');
+//  }
+// document.querySelector('.pp-close').addEventListener('click' , togglePortfolioPopup);
 
-//hide popup when click outside of it
-document.addEventListener('click' , (e)=>{
-    if(e.target.classList.contains('pp-inner')){
-        togglePortfolioPopup();
-        document.querySelector(".portfolio-popup").scrollTo(0,0);
-        portfolioItemDetails(e.target.parentElement);
-    }
-})
-function portfolioItemDetails(portfolioItem) {
-    document.querySelector('.pp-thumbnail img').src = 
-    portfolioItem.querySelector('.portfolio-item-thumbnail').src;
-    document.querySelector('.pp-header h3').innerHTML =
-    portfolioItem.querySelector('.portfolio-item-title').innerHTML;
-    document.querySelector('.pp-body').innerHTML =
-    portfolioItem.querySelector('.portfolio-item-details').innerHTML;
+// //hide popup when click outside of it
+// document.addEventListener('click' , (e)=>{
+//     if(e.target.classList.contains('pp-inner')){
+//         togglePortfolioPopup();
+//         document.querySelector(".portfolio-popup").scrollTo(0,0);
+//        // portfolioItemDetails(e.target.parentElement);
+//     }
+// })
+// function portfolioItemDetails(portfolioItem) {
+//     document.querySelector('.pp-thumbnail img').src = 
+//     portfolioItem.querySelector('.portfolio-item-thumbnail').src;
+//     document.querySelector('.pp-header h3').innerHTML =
+//     portfolioItem.querySelector('.portfolio-item-title').innerHTML;
+//     document.querySelector('.pp-body').innerHTML =
+//     portfolioItem.querySelector('.portfolio-item-details').innerHTML;
 
-}
+// }
 
 // -------------Toggle Navbar-------------
 const navToggler = document.querySelector('.nav-toggler');
